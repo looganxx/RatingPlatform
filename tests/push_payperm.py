7b6b4b915d650bcc1ab1b83d99c098e295b165b8
looganxx <giuseppe1398@gmail.com>
2020-12-07 15:59:37 +0100

    test transaction

--- tests/push_payperm.py
+++ tests/push_payperm.py
@@ -5,119 +5,49 @@ from eospy.utils import parse_key_file
 import os
 import pytz
 import json
+import yaml
 
 ce = eospy.cleos.Cleos(url='https://jungle3.cryptolions.io:443')
 
-arguments = {
-    "skill_name":"tradizionale"
-}
-payload = {
-    "account": "ratingsystem",
-    "name": "addskill",
-    "authorization": [{
-        "actor": "ratingsystem",
-        "permission": "active",
-    }],
-}
-#Converting payload to binary
-data = ce.abi_json_to_bin(payload['account'], payload['name'], arguments)
-#Inserting payload binary form as "data" field in original payload
-payload['data'] = data['binargs']
-#final transaction formed
-trx = {"actions": [payload]}
-import datetime as dt
-trx['expiration'] = str(
-    (dt.datetime.utcnow() + dt.timedelta(seconds=60)).replace(tzinfo=pytz.UTC))
-# use a string or EOSKey for push_transaction
-# key = "5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3"
-# use EOSKey:
-key = eospy.keys.EOSKey('5J68cwLYxk6QWE3kdnYBbW1eNYKYTT2g7U6QFBY1eh3p5zo87Sc')
-resp = ce.push_transaction(trx, key, broadcast=True)
+bill=0.50
+for counter in range(349):
+	bill = ((bill + 0.5)%1)+1
+	arguments = {
+		"item": "sushi",
+		"owner": "alicersfacco",
+		"client": "bobrsfaccoun",
+		"bill": "{price:.2f} RSF".format(price=bill)
+	}
+	payload = {
+		"account": "ratingsystem",
+		"name": "payperm",
+		"authorization": [{
+				"actor": "alicersfacco",
+				"permission": "active",
+		}],
+	}
+	#Converting payload to binary
+	data = ce.abi_json_to_bin(payload['account'], payload['name'], arguments)
+	#Inserting payload binary form as "data" field in original payload
+	payload['data'] = data['binargs']
+	#final transaction formed
+	trx = {"actions": [payload]}
+	import datetime as dt
+	trx['expiration'] = str(
+		(dt.datetime.utcnow() + dt.timedelta(seconds=60)).replace(tzinfo=pytz.UTC))
+
+	key = eospy.keys.EOSKey('5Keb8LCEwfSCBinCn2AcN8KQBKSjJ7inoJnUJo6aSHMMJvGSiN7')
+
+	with open("payperm.log", "a") as log:
+		resp = ce.push_transaction(trx, key, broadcast=True)
+		#print (resp)
+		delta_ram = resp['processed']['action_traces'][0]['account_ram_deltas'][0]
+		# Append text at the end of file
+		log.writelines([delta_ram['account'], ": " , "{}".format(delta_ram['delta']), "\n" ])
+
+"""
 print('------------------------------------------------')
 print(resp)
+print (yaml.dump(resp, default_flow_style=False))
 print('------------------------------------------------')
-
-#ratingsystem key
-script_dir = os.path.dirname(os.path.realpath(__file__))
-key_file = os.path.join(script_dir, 'rsfkeys.key')
-key = parse_key_file(key_file)
-
-#find abi file of contract
-os.chdir('../contracts/RatingSystem/')
-abi_dir = os.getcwd()
-abi_file = os.path.join(abi_dir, 'RatingSystem.abi')
-
-with open(abi_file) as rf:
-    abi_json = json.load(rf)
-    token_abi = Abi(abi_json)
-
-    # wait to send transaction
-    import time
-    print('Pushing a transaction after 2 seconds using abi file: {}'.format(
-        abi_file))
-    time.sleep(2)
-
-    act_params = {
-        "skill_name":"tradizionale"
-    }
-    #Converting payload to binary
-    data = ce.abi_json_to_bin(payload['account'], payload['name'], act_params)
-    print(data)
-    act_data = token_abi.json_to_bin(payload['name'], act_params)
-    print(act_data)
-    #Inserting payload binary form as "data" field in original payload
-    payload['data'] = act_data
-    #final transaction formed
-    trx = {"actions": [payload]}
-
-    resp = ce.push_transaction(trx, eospy.keys.EOSKey(key), broadcast=True)
-    print('------------------------------------------------')
-    print(resp)
-    print('------------------------------------------------')
-
-########################################
-# VOTE
-########################################
 """
-abi_file = os.path.join(script_dir, 'eosio.system.abi')
-with open(abi_file) as rf:
-    abi_json = json.load(rf)
-    sys_abi = Abi(abi_json)
-
-    # wait to send transaction
-    import time
-    print('Pushing a vote after 2 seconds using abi file: {}'.format(abi_file))
-    time.sleep(2)
-
-    vote_payload = {
-        "account": "eosio",
-        "name": "voteproducer",
-        "authorization": [{
-            "actor": "dspnewyorkio",
-            "permission": "active",
-        }],
-    }
-
-    vote_params = {
-        "voter": "dspnewyorkio",  # sender
-        "proxy": "",  # proxy
-        "producers": ['bpa1', 'bpa2']  # producers
-    }
-
-    # Voting Converting payload to binary
-    data = ce.abi_json_to_bin(vote_payload['account'], vote_payload['name'],
-                              vote_params)
-    print(data)
-    vote_data = sys_abi.json_to_bin(vote_payload['name'], vote_params)
-    print(vote_data)
-    # Inserting payload binary form as "data" field in original payload
-    vote_payload['data'] = vote_data
-
-    # final transaction formed
-    trx = {"actions": [vote_payload]}
-
-    resp = ce.push_transaction(trx, eospy.keys.EOSKey(key), broadcast=True)
-    print('------------------------------------------------')
-    print(resp)
-    print('------------------------------------------------')
-    """
